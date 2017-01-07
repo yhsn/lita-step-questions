9d2d0bc31503e528f88277d2e83d73dcda796542
yhsn <yhsn@users.noreply.github.com>
2017-01-08 00:13:56 +0900

    bug fix + cleaning

--- lib/lita-step-questions.rb
+++ lib/lita-step-questions.rb
@@ -1,10 +1,10 @@
-require "lita"
+require 'lita'
 
 Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
+  File.join('..', '..', 'locales', '*.yml'), __FILE__
 )]
 
-require "lita/extensions/step_questions"
-require "lita/extensions/step_questions/named_redis"
-require "lita/extensions/step_questions/base"
-require "lita/extensions/step_questions/handler"
+require 'lita/extensions/step_questions'
+require 'lita/extensions/step_questions/named_redis'
+require 'lita/extensions/step_questions/base'
+require 'lita/extensions/step_questions/handler'
