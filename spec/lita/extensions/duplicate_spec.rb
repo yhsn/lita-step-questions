aeb50c7e8289f7826e7ae3e97ce2a5943cb9074e
yhsn <yhsn@users.noreply.github.com>
2017-01-28 12:18:03 +0900

    prevent duplicate session

---
+++ spec/lita/extensions/duplicate_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe SampleHandler, lita_handler: true, additional_lita_handlers: Lita::Extensions::StepQuestions::Handler do
+  before do
+    registry.register_hook(:validate_route, Lita::Extensions::StepQuestions)
+    LastSelectQuestion.clear_all(user.id)
+  end
+
+  context 'receive command two times' do
+    before do
+      send_command('order')
+      send_command('message')
+    end
+
+    it 'not start second session' do
+      expect(replies.last).not_to include '(1/2)One:'
+    end
+  end
+end
