5b45882ee6c978d10cbb43e38c567f85aed4919c
hoge <hoge@example.com>
2017-02-01 00:01:28 +0900

    cleaning by rubycritic

--- lib/lita/extensions/step_questions/handler.rb
+++ lib/lita/extensions/step_questions/handler.rb
@@ -1,19 +1,19 @@
 module Lita
   module Extensions
     class StepQuestions
+      # Handling message from user
+      # Process message body and control question flow
       class Handler < Lita::Handler
         on :message_dispatched, :all
         on :unhandled_message, :all
 
         def all(payload)
-          m = payload[:message]
-          q = Lita::Extensions::StepQuestions.current m
+          q = Lita::Extensions::StepQuestions.current payload[:message]
 
           return true unless q
 
           if q.receive_answer
             q.finish unless q.next!
-            return
           elsif q.aborting?
             q.confirm_abort
           end
